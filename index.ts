22e06541d4542856064a80770a71a29cf10c2066
Timo Varila <timo.varila@siili.com>
2020-05-19 12:37:42 +0300

    client build

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('client'));
+app.use(express.static('build'));
 
 app.get('/ping', (_req, res) => {
   console.log('someone pings');
