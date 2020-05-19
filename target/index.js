43cf986952afb83d7c8a38f74d0bab4fddde7f66
Timo Varila <timo.varila@siili.com>
2020-05-19 12:48:14 +0300

    client directory corrected

--- target/index.js
+++ target/index.js
@@ -8,7 +8,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use(cors_1.default());
-app.use(express_1.default.static('client'));
+app.use(express_1.default.static('build'));
 app.get('/ping', (_req, res) => {
     console.log('someone pings');
     res.send('pong');
